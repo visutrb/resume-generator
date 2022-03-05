227048227c9c076f040c1aa26ecf38facc6a446a
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-05 23:23:33 +0700

    Annotate ResumeDao with @Dao

--- app/src/main/kotlin/me/visutrb/resumegen/db/dao/ResumeDao.kt
+++ app/src/main/kotlin/me/visutrb/resumegen/db/dao/ResumeDao.kt
@@ -1,10 +1,12 @@
 package me.visutrb.resumegen.db.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import me.visutrb.resumegen.entity.Resume
 
+@Dao
 interface ResumeDao {
 
     @Query("SELECT * FROM resume")
