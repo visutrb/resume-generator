9e9ef011e4793d115f141d39b15cc651c089c53f
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-06 00:11:15 +0700

    Add methods providing the remaining daos

--- app/src/main/kotlin/me/visutrb/resumegen/db/AppDatabase.kt
+++ app/src/main/kotlin/me/visutrb/resumegen/db/AppDatabase.kt
@@ -2,7 +2,7 @@ package me.visutrb.resumegen.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import me.visutrb.resumegen.db.dao.ResumeDao
+import me.visutrb.resumegen.db.dao.*
 import me.visutrb.resumegen.entity.*
 
 @Database(
@@ -17,5 +17,10 @@ import me.visutrb.resumegen.entity.*
     version = 1
 )
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun educationDao(): EducationDao
+    abstract fun projectDao(): ProjectDao
     abstract fun resumeDao(): ResumeDao
+    abstract fun skillDao(): SkillDao
+    abstract fun technologyDao(): TechnologyDao
+    abstract fun workExperienceDao(): WorkExperienceDao
 }
