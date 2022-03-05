9528f2706b15f34d5efe5bb3ea7ef75af32c2f6e
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-05 18:09:13 +0700

    Annotate WorkExperience with @Entity

--- app/src/main/kotlin/me/visutrb/resumegen/entity/WorkExperience.kt
+++ app/src/main/kotlin/me/visutrb/resumegen/entity/WorkExperience.kt
@@ -1,7 +1,9 @@
 package me.visutrb.resumegen.entity
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class WorkExperience(
     @PrimaryKey val id: Int,
     val companyName: String,
