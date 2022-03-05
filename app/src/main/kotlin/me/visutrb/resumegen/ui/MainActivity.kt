51931066d0cb02687f88feaf3929d41ad09d9512
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-06 01:38:34 +0700

    Relocate MainActivity

---
+++ app/src/main/kotlin/me/visutrb/resumegen/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.visutrb.resumegen.ui
+
+import android.os.Bundle
+import me.visutrb.resumegen.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
