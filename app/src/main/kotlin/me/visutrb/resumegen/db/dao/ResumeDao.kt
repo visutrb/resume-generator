fd50c97368e4118fad5e07327508e8530dd619c6
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-05 23:40:20 +0700

    Update ResumeDao, add methods to load resume with relations

--- app/src/main/kotlin/me/visutrb/resumegen/db/dao/ResumeDao.kt
+++ app/src/main/kotlin/me/visutrb/resumegen/db/dao/ResumeDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import me.visutrb.resumegen.db.dto.ResumeWithRelations
 import me.visutrb.resumegen.entity.Resume
 
 @Dao
@@ -12,8 +13,14 @@ interface ResumeDao {
     @Query("SELECT * FROM Resume")
     fun findAll(): List<Resume>
 
+    @Query("SELECT * FROM Resume")
+    fun findAllWithRelations(): List<ResumeWithRelations>
+
     @Query("SELECT * FROM Resume WHERE id = :id")
     fun findById(id: Int): Resume
+
+    @Query("SELECT * FROM Resume WHERE id = :id")
+    fun findByIdWithRelations(id: Int): ResumeWithRelations
 
     @Insert
     fun insertAll(vararg resumes: Resume)
