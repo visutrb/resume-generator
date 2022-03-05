b348cfa69eccf23cb88b70137c1bb2370ecd5426
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-06 04:53:04 +0700

    Setup UI components in resume list activity

--- app/src/main/kotlin/me/visutrb/resumegen/ui/resumelist/ResumeListActivity.kt
+++ app/src/main/kotlin/me/visutrb/resumegen/ui/resumelist/ResumeListActivity.kt
@@ -1,16 +1,32 @@
 package me.visutrb.resumegen.ui.resumelist
 
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import me.visutrb.resumegen.databinding.ActivityResumeListBinding
 import me.visutrb.resumegen.ui.BaseActivity
 
 class ResumeListActivity : BaseActivity() {
 
     private lateinit var binding: ActivityResumeListBinding
+    private lateinit var recyclerViewAdapter: ResumeListRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResumeListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        recyclerViewAdapter = ResumeListRecyclerViewAdapter()
+
+        binding.resumeRv.apply {
+            adapter = recyclerViewAdapter
+            layoutManager = GridLayoutManager(this@ResumeListActivity, 2)
+        }
+
+        binding.createResumeBtn.setOnClickListener {
+            TODO("Start new resume activity")
+        }
+
+        binding.noResumeTv.visibility = View.VISIBLE
     }
 }
