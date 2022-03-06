595071c02198cb35bd2d99fe22021ed25cd21060
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-06 14:59:20 +0700

    Create base presenter

---
+++ app/src/main/kotlin/me/visutrb/resumegen/mvp/Presenter.kt
@@ -0,0 +1,17 @@
+package me.visutrb.resumegen.mvp
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+abstract class Presenter<View : Presenter.View> : CoroutineScope {
+
+    protected val job = Job()
+    override val coroutineContext: CoroutineContext = Job() + Dispatchers.Main
+
+    var view: View? = null
+        protected set
+
+    interface View
+}
