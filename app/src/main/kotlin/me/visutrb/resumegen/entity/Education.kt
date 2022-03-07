0d6314844b4ea7403a5d60716bdfb807d604ed72
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-07 23:03:49 +0700

    Make Education parcelable

--- app/src/main/kotlin/me/visutrb/resumegen/entity/Education.kt
+++ app/src/main/kotlin/me/visutrb/resumegen/entity/Education.kt
@@ -1,13 +1,18 @@
 package me.visutrb.resumegen.entity
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity
+@Parcelize
 data class Education(
-    @PrimaryKey val id: Int,
-    val classRank: String,
-    val passingYear: Int,
-    val cgpa: String,
-    val resumeId: Int
-)
+    @PrimaryKey val id: Long = 0,
+    val school: String,
+    val degree: String,
+    val fos: String,
+    val graduateYear: Int,
+    val cgpa: Float,
+    val resumeId: Long = 0
+) : Parcelable
