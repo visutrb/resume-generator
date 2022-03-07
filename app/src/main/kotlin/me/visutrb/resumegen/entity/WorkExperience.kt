e5e651c84af93d6bc7a80f276b399be452066fb3
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-08 00:38:21 +0700

    Set default value for id fields

--- app/src/main/kotlin/me/visutrb/resumegen/entity/WorkExperience.kt
+++ app/src/main/kotlin/me/visutrb/resumegen/entity/WorkExperience.kt
@@ -1,15 +1,19 @@
 package me.visutrb.resumegen.entity
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity
+@Parcelize
 data class WorkExperience(
-    @PrimaryKey val id: Int,
+    @PrimaryKey val id: Long = 0,
     val companyName: String,
+    val role: String,
     val startMonth: Int,
     val startYear: Int,
     val endMonth: Int = -1,
-    val endYear: Int = 0,
-    val resumeId: Int
-)
+    val endYear: Int = -1,
+    val resumeId: Long = 0
+) : Parcelable
