cbdfeeb018506f25b8bfa4d4ba8ebe905127c33c
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-07 23:13:47 +0700

    Make Project Parcelable, update id properties to be long

--- app/src/main/kotlin/me/visutrb/resumegen/entity/Project.kt
+++ app/src/main/kotlin/me/visutrb/resumegen/entity/Project.kt
@@ -1,14 +1,17 @@
 package me.visutrb.resumegen.entity
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity
+@Parcelize
 data class Project(
-    @PrimaryKey val id: Int,
+    @PrimaryKey val id: Long = 0,
     val name: String,
     val teamSize: Int,
     val summary: String,
     val role: String,
-    val resumeId: Int
-)
+    val resumeId: Long
+) : Parcelable
