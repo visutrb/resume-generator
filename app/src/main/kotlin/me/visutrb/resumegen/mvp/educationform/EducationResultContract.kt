4e869ba07e8df902a71c44de1a103d3db8ce66b7
Visutr Boonnateephisit <visutr.boon@gmail.com>
2022-03-07 23:05:55 +0700

    Implement EducationResultContract

---
+++ app/src/main/kotlin/me/visutrb/resumegen/mvp/educationform/EducationResultContract.kt
@@ -0,0 +1,21 @@
+package me.visutrb.resumegen.mvp.educationform
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+import me.visutrb.resumegen.entity.Education
+
+class EducationResultContract : ActivityResultContract<Education?, Education?>() {
+
+    override fun createIntent(context: Context, input: Education?): Intent {
+        return EducationFormActivity.newIntent(context, input)
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): Education? {
+        if (resultCode != Activity.RESULT_OK) {
+            return null
+        }
+        return intent?.let { it.getParcelableExtra(EducationFormActivity.EXTRA_EDUCATION) as Education? }
+    }
+}
